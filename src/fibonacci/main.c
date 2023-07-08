1e728501af2be7c2a09abe3f6529376631541cce
Chris Cabang <ccscabang@gmail.com>
2023-07-08 09:17:32 +0800

    feat: fibonacci study with recursion

--- src/fibonacci/main.c
+++ src/fibonacci/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+int fibonacci(int n) {
+  if(n == 0 || n == 1) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n -2);
+}
+
+void print_fibonacci_series(int n) {
+  for (int i = 0; i <= n; i++) {
+    printf("%d ", fibonacci(i));
+  }
+}
+
 int main() {
+  int n = 6;
   
+  printf("The %dth fibonacci number is %d: [ ", n, fibonacci(n));
+  print_fibonacci_series(n);
+  printf("]\n");
+
   return 0;
 }
