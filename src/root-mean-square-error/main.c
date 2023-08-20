04459ff02e5e05afac68ad3c57c337f4390ef9f0
Chris Cabang <ccscabang@gmail.com>
2023-08-20 14:05:51 +0800

    feat: prints the predicted and observed arrays

--- src/root-mean-square-error/main.c
+++ src/root-mean-square-error/main.c
@@ -3,6 +3,12 @@
 
 #define square(x) (x * x)
 
+void print_array(double arr[], int size) {
+  for (int i = 0; i < size; i++) {
+      printf("%2.2f ", arr[i]);
+  }
+}
+
 double root_mean_square_error(double predicted[], double observed[], int N) {
     // RMSE = sqrt( (square(y1) + square(y2) + ... + square(yn)) / length)
     //  yn = predicted[n] - observed[n]
@@ -22,11 +28,18 @@ double root_mean_square_error(double predicted[], double observed[], int N) {
 int main() {
     double predicted[] = {4, 25,  0.75, 11};
     double observed[] = {3, 21, -1.25, 13};
-    
+
     int length = sizeof(predicted)/sizeof(predicted[0]);
 
     double rmse = root_mean_square_error(predicted, observed, length);
-    
+    printf("Predicted: { "); {
+        print_array(predicted, sizeof(predicted)/sizeof(predicted[0])); 
+        printf(" }\n");
+    }
+    printf("Observed : { "); {
+        print_array(observed, sizeof(observed)/sizeof(observed[0])); 
+        printf(" }\n");
+    }
     printf("Root-Mean-Square Error: %.3lf\n", rmse);
     
     return 0;
